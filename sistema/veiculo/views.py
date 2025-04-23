8555c7b23a727ebc63bc34208f42e783a1010f39
JaumMota <joao.mota1@mail.uft.edu.br>
2025-04-23 20:41:00 -0300

    teste

--- sistema/veiculo/views.py
+++ sistema/veiculo/views.py
@@ -1,8 +1,10 @@
 from veiculo.models import Veiculo
-from django.views.generic import ListView
+from django.views.generic import ListView 
 
 class ListarVeiculos(ListView):
-    
+    """
+    View para listar todos os veiculos cadastrados.
+    """
     model = Veiculo 
     context_object_name = 'veiculos'
     template_name = 'veiculo/listar.html'
