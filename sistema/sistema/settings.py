3722fd0526dd7097eb797b1f214730166ede154b
JaumMota <joao.mota1@mail.uft.edu.br>
2025-05-15 11:17:17 -0300

    aula 14.05

--- sistema/sistema/settings.py
+++ sistema/sistema/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_bootstrap5',
-    'veiculo.apps.VeiculoConfig'
+    'veiculo.apps.VeiculoConfig',
+    'anuncio.apps.AnuncioConfig',
 ]
 
 MIDDLEWARE = [
--- sistema/veiculo/views.py
+++ sistema/veiculo/views.py
@@ -1,10 +1,14 @@
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import FileResponse, Http404
+from django.views import View
 from veiculo.models import Veiculo
 from veiculo.forms import FormularioVeiculo
 from django.views.generic import ListView
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-class ListarVeiculos(ListView):
+class ListarVeiculos(LoginRequiredMixin, ListView):
     """
     View para listar todos os veiculos cadastrados.
     """
@@ -12,11 +16,44 @@ class ListarVeiculos(ListView):
     context_object_name = 'veiculos'
     template_name = 'veiculo/listar.html'
 
-class CriarVeiculos(CreateView):
+class CriarVeiculos(LoginRequiredMixin, CreateView):
     """
     View para a criação de novos veiculos.
     """
     model = Veiculo
     form_class = FormularioVeiculo
     template_name = 'veiculo/novo.html'
+    success_url = reverse_lazy('listar-veiculos')
+
+class FotoVeiculo(View):
+    """
+    View para retornar a foto de um veiculo.
+    """
+    def get(self, request, arquivo):
+        try:
+            veiculo = Veiculo.objects.get(foto='veiculo/fotos/{}'.format(arquivo))
+            return FileResponse(veiculo.foto)
+        except ObjectDoesNotExist:
+            return Http404('Foto não encontrada ou acesso negado.')
+        except Exception as exception:
+            return exception
+        
+
+class EditarVeiculos(LoginRequiredMixin, UpdateView):
+
+    """
+    View para editar veiculos ja cadastrados.
+    """
+    
+    model = Veiculo
+    form_class = FormularioVeiculo
+    template_name = 'veiculo/editar.html'
+    success_url = reverse_lazy('listar-veiculos')
+
+class DeletarVeiculos(LoginRequiredMixin, DeleteView):
+    """
+    View para deletar veiculos ja cadastrados.
+    """
+    model = Veiculo
+    template_name = 'veiculo/deletar.html'
     success_url = reverse_lazy('listar-veiculos')
